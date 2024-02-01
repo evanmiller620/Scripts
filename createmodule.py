674bd5cf88342ee39bdfe67c1fbe0a7b561f7662
evan11 <71155890+evanmiller620@users.noreply.github.com>
2024-02-01 14:09:26 -0500

    Update createmodule.py
    Added reg and comb

--- createmodule.py
+++ createmodule.py
@@ -1,6 +1,4 @@
 #Script to make systemVerilog modules quickly for ece337 and 437 lab
-
-
 name = input("Enter module name: ")
 filename = name + ".sv"
 f = open(filename, "w")
@@ -13,7 +11,6 @@ bits = ""
 while(user != "n"):
     if(user[-1] == "]"):
         idx = user.find("[")
-        print(idx)
         bits = user[idx:]
         user = user[0:idx]
     else:
@@ -27,13 +24,15 @@ user = input("output? (n for done): ")
 while(user != "n"):
     if(user[-1] == "]"):
         idx = user.find("[")
-        print(idx)
         bits = user[idx:]
         user = user[0:idx]
     else:
         bits = ""
     f.write(",\n\toutput logic " + bits + " " + user)
     user = input("output? (n for done): ")
-    
-f.write("\n);\n\nendmodule")
+
+f.write("\n);\n\talways_ff @(posedge clk, negedge n_rst) begin\n\t\t\n\tend\n") #registers
+f.write("\n\talways_comb begin\n\t\t\n\tend\n") #comb logic
+f.write("endmodule")
 f.close
+
